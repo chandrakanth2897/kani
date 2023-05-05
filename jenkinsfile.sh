ef87a7d6708c654704b5560bd4d833b722752cb0
chandrakanth2897 <121753583+chandrakanth2897@users.noreply.github.com>
2023-05-05 22:06:17 +0530

    Update and rename jenkinsfile to jenkinsfile.sh

--- jenkinsfile.sh
+++ jenkinsfile.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
-sudo apt install openjdk-11-jdk
+sudo apt install openjdk-11-jdk -y
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
